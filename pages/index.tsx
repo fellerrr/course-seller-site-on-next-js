944dfc5a710df0c6718d19732eb104b3d75a47de
fellerrr <fellerrr@ya.ru>
2023-02-17 23:47:36 +0300

    rating component added svg as component with style added

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,13 +1,18 @@
 import  Htag  from '../components/Htag/Htag';
 import Button from "../components/Button/Button";
-import P from "../components/P/P"
+import P from "../components/P/P";
 import Tag from "../components/Tag/Tag";
+import {useState} from "react";
+import Rating from "../components/Rating/Rating";
 
 export default function Home() {
+    const [count, setCount] = useState<number>(0);
+
+    const [rating,setRaring]=useState<number>(1);
   return (
-    <div className='container'>
-        <Htag tag="h3">Текст</Htag>
-        <Button appearance={"primary"} arrow={"right"}>Кнопка</Button>
+    <div className='container test'>
+        <Htag tag="h3">На счетчике уже {count} ДолларОв</Htag>
+        <Button appearance={"primary"} arrow={"right"} onClick={()=>setCount(count+1)}>Кнопка</Button>
         <Button appearance={"ghost"} arrow={"down"}>Кнопка2</Button>
         <P size={"l"}>Большой</P>
         <P>Средний</P>
@@ -18,6 +23,7 @@ export default function Home() {
         <Tag size={'m'} color={'green'}>green</Tag>
         <Tag size={'m'} color={'primary'}>primary</Tag>
         <Tag color={'primary'}>primary</Tag>
+        <Rating rating={rating} isEditable setRating={setRaring}/>
     </div>
   );
 }
